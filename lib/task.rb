a21862d929451e99947ee6a885f520824bea0ff9
brooke perry and maly phongsavanh <brooke@>
2018-12-10 11:12:01 -0800

    shorten code with active records::base

---
+++ lib/task.rb
@@ -0,0 +1,11 @@
+class Task < ActiveRecord::Base
+  belongs_to(:list)
+
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
+end
+# list = List.new({:name => 'Epicodus stuff'})
+# task = list.tasks.new({:description => "learn Active Record"})
+# list.save
+# task.list
